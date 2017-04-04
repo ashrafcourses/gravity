9ccb8343bf40cb0e554db83be854351371731edb
LLLZhao <1005570863@qq.com>
2017-04-04 13:58:02 -0300

    change the gravity constant and get the value

--- AbstractPendulum.java
+++ AbstractPendulum.java
@@ -4,7 +4,6 @@ package dal.gravity;
  * Represents a pendulum
  */
 public abstract class AbstractPendulum {
-
 	
     /* instance variables - string length, point mass, angular displacement
      * at t=0, constant for local gravitational field in m/s^2 (e.g., 9.81 on Earth)
